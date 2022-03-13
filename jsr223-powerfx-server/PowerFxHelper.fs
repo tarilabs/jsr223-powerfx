43782259d950b56ecf229cc574bc1656c729e39f
tarilabs <matteo.mortari@gmail.com>
2022-03-13 11:53:30 +0100

    covering FormulaValue to string

--- jsr223-powerfx-server/PowerFxHelper.fs
+++ jsr223-powerfx-server/PowerFxHelper.fs
@@ -1,10 +1,43 @@
 module jsr223_powerfx_server.PowerFxHelper
 
-open Microsoft.PowerFx.Core.Public.Values;
+open Microsoft.PowerFx.Core.Public.Values
+open System
+open System.Linq
 
-let valToString (v : FormulaValue): string =
+let dRecToFormula (x: DValue<RecordValue>) = x.ToFormulaValue()
+let dRecColumnToFormula (x: DValue<RecordValue>) = x.Value.Fields.First().Value
+
+let rec valToString (v : FormulaValue): string =
     match v with
     | :? NumberValue as x -> $"{x.Value}"
     | :? BooleanValue as x -> $"{x.Value}"
-    | :? StringValue as x -> $"\"{x.Value}\""
-    | _ -> "TODO"
+    | :? StringValue as x -> "\"" + x.Value.Replace("\"", "\\\"") + "\""
+    | :? TableValue as t ->
+        if t.IsColumn then
+            let x =
+                t.Rows
+                |> Seq.map dRecColumnToFormula
+                |> Seq.map valToString
+                |> Seq.reduce (fun a b -> a + "," + b)
+            "[" + x + "]"
+        else
+            let x =
+                t.Rows
+                |> Seq.map dRecToFormula
+                |> Seq.map valToString
+                |> Seq.reduce (fun a b -> a + "," + b)
+            "[" + x + "]"
+    | :? RecordValue as r ->
+        let fields = r.Fields.ToArray() // we want to react on size
+        if fields.Length = 1 && fields[0].Name = "Value" then
+            "[" + valToString(fields[0].Value) + "]"
+        else
+            let sorted =
+                fields
+                |> Array.sortWith (fun a b -> String.CompareOrdinal(a.Name, b.Name))
+                |> Array.map (fun x -> x.Name + ":" + valToString(x.Value))
+                |> Seq.reduce (fun a b -> a + "," + b)
+            "{" + sorted + "}"
+    | :? BlankValue -> "{}"
+    | _ -> "\"<unknown>\""
+
