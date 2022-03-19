54899a0caa37272a107811fd80b4a59f8f6a851b
tarilabs <matteo.mortari@gmail.com>
2022-03-19 20:03:12 +0100

    F# server PowerFx FormulaValue parsing refactoring

--- jsr223-powerfx-server/PowerFxHelper.fs
+++ jsr223-powerfx-server/PowerFxHelper.fs
@@ -7,37 +7,41 @@ open System.Linq
 let dRecToFormula (x: DValue<RecordValue>) = x.ToFormulaValue()
 let dRecColumnToFormula (x: DValue<RecordValue>) = x.Value.Fields.First().Value
 
+let tableValueToString (t: TableValue) (recFn: FormulaValue -> string) = 
+    if t.IsColumn then
+        let x =
+            t.Rows
+            |> Seq.map dRecColumnToFormula
+            |> Seq.map recFn
+            |> Seq.reduce (fun a b -> a + "," + b)
+        "[" + x + "]"
+    else
+        let x =
+            t.Rows
+            |> Seq.map dRecToFormula
+            |> Seq.map recFn
+            |> Seq.reduce (fun a b -> a + "," + b)
+        "[" + x + "]"
+
+let recordValue (r: RecordValue) (recFn: FormulaValue -> string) = 
+    let fields = r.Fields.ToArray() // we want to react on size
+    if fields.Length = 1 && fields[0].Name = "Value" then
+        "[" + recFn(fields[0].Value) + "]"
+    else
+        let sorted =
+            fields
+            |> Array.sortWith (fun a b -> String.CompareOrdinal(a.Name, b.Name))
+            |> Array.map (fun x -> x.Name + ":" + recFn(x.Value))
+            |> Seq.reduce (fun a b -> a + "," + b)
+        "{" + sorted + "}"
+
 let rec valToString (v : FormulaValue): string =
     match v with
     | :? NumberValue as x -> $"{x.Value}"
     | :? BooleanValue as x -> $"{x.Value}"
     | :? StringValue as x -> "\"" + x.Value.Replace("\"", "\\\"") + "\""
-    | :? TableValue as t ->
-        if t.IsColumn then
-            let x =
-                t.Rows
-                |> Seq.map dRecColumnToFormula
-                |> Seq.map valToString
-                |> Seq.reduce (fun a b -> a + "," + b)
-            "[" + x + "]"
-        else
-            let x =
-                t.Rows
-                |> Seq.map dRecToFormula
-                |> Seq.map valToString
-                |> Seq.reduce (fun a b -> a + "," + b)
-            "[" + x + "]"
-    | :? RecordValue as r ->
-        let fields = r.Fields.ToArray() // we want to react on size
-        if fields.Length = 1 && fields[0].Name = "Value" then
-            "[" + valToString(fields[0].Value) + "]"
-        else
-            let sorted =
-                fields
-                |> Array.sortWith (fun a b -> String.CompareOrdinal(a.Name, b.Name))
-                |> Array.map (fun x -> x.Name + ":" + valToString(x.Value))
-                |> Seq.reduce (fun a b -> a + "," + b)
-            "{" + sorted + "}"
+    | :? TableValue as t -> tableValueToString t valToString
+    | :? RecordValue as r -> recordValue r valToString
     | :? BlankValue -> "{}"
     | _ -> "\"<unknown>\""
 
